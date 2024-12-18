52dc0061968455f61f04f6fa2a73a0dabf73b122
Jeremy lorette <jeremy.lorette@outlook.com>
2024-12-18 16:25:50 +0100

    fix cleanup_sim

--- philo/src/memory/free.c
+++ philo/src/memory/free.c
@@ -6,7 +6,7 @@
 /*   By: jlorette <jlorette@42angouleme.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/10 14:34:17 by jlorette          #+#    #+#             */
-/*   Updated: 2024/12/10 14:34:30 by jlorette         ###   ########.fr       */
+/*   Updated: 2024/12/18 16:23:43 by jlorette         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,23 @@
 
 void	cleanup_sim(t_sim *sim)
 {
-	int	i;
+    int	i;
 
-	if (sim->philos)
-		free(sim->philos);
-	if (sim->forks)
-	{
-		i = 0;
-		while (i < sim->args->philo_count)
-		{
-			pthread_mutex_destroy(&sim->forks[i]);
-			i++;
-		}
-		free(sim->forks);
-	}
-	pthread_mutex_destroy(&sim->write_lock);
-	free(sim);
+    if (!sim)
+        return;
+    if (sim->philos)
+        free(sim->philos);
+    if (sim->forks)
+    {
+        i = 0;
+        while (i < sim->args->philo_count)
+        {
+            pthread_mutex_destroy(&sim->forks[i]);
+            i++;
+        }
+        free(sim->forks);
+    }
+    pthread_mutex_destroy(&sim->write_lock);
+    pthread_mutex_destroy(&sim->death_mutex);
+    free(sim);
 }
